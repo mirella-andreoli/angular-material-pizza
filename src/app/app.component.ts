ddcddb0a25b21c9029a8f584c979aa844628a4cb
mirella-andreoli <mirella.andreoli@hotmail.com>
2018-05-19 18:48:36 -0300

    teste-material-angular
    pessoas e menu - syles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 
-import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, ViewChild} from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import {MatToolbar} from '@angular/material/toolbar';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,11 +11,12 @@ import { MatSidenav } from '@angular/material/sidenav';
 export class AppComponent {
   @ViewChild('nav') nav: MatSidenav;
 
+
   close() {
     this.nav.close();
   }
 
-  itens = Array(50).fill(0).map((_, i) => `Nav Item ${i + 1}`);
+  itens = Array(50).fill(0).map((_, i) => `Nav Iteeeeeeeeeeeeeeeeeeeeeeeeeeem ${i + 1}`);
 
 
 }
